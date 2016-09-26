df3cd64f29853a469986ae3ee67c6a4fa2df84d3
Tim Reynolds <tim@timothyreynolds.co.uk>
2016-09-26 15:19:14 +0100

    Updated toMomentObject to take advantage of the fact moment takes an array of formats
    Moment can take an array of formats as outlined in
    http://momentjs.com/docs/#/parsing/special-formats/
    
    Using this makes the toMomentObject util easier to understand as it
    reduces if return statements within the method.
    
    All tests still pass with the refactor

--- src/utils/toMomentObject.js
+++ src/utils/toMomentObject.js
@@ -3,16 +3,10 @@ import moment from 'moment';
 import { DISPLAY_FORMAT, ISO_FORMAT } from '../../constants';
 
 export default function toMomentObject(dateString, customFormat) {
-  if (customFormat) {
-    const customFormatDate = moment(dateString, customFormat, true);
-    if (customFormatDate.isValid()) return customFormatDate;
-  }
+  const dateFormats = customFormat ?
+    [customFormat, DISPLAY_FORMAT, ISO_FORMAT] :
+    [DISPLAY_FORMAT, ISO_FORMAT];
 
-  const date = moment(dateString, DISPLAY_FORMAT, true);
-  if (date.isValid()) return date;
-
-  const isoDate = moment(dateString, ISO_FORMAT, true);
-  if (isoDate.isValid()) return isoDate;
-
-  return null;
+  const date = moment(dateString, dateFormats, true);
+  return date.isValid() ? date : null;
 }
