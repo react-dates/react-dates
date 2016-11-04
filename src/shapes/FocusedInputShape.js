99bde9fabc12295731404fcf084c0ff141a0ba5a
Corey Light <coreylight@gmail.com>
2016-11-04 12:56:12 -0500

    Use stricter prop types to decrease confusion for new users, related to https://github.com/airbnb/react-dates/issues/126

--- src/shapes/FocusedInputShape.js
+++ src/shapes/FocusedInputShape.js
@@ -5,4 +5,10 @@ import {
   END_DATE,
 } from '../../constants';
 
-export default PropTypes.oneOf([START_DATE, END_DATE]);
+export default function(props, propName, componentName){
+  const item = props[propName];
+  if (item === START_DATE || item === END_DATE || item === ''){
+    return null;
+  }
+  return new Error(`${propName} must be an empty string, "${START_DATE}", or "${END_DATE}"`);
+}
