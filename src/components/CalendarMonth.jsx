c4fb866b0a7f5f4e4101141ed6a1ae43ed75db64
Maja Wichrowska <maja.wichrowska@airbnb.com>
2016-12-16 14:37:29 -0800

    Maintains an array of moment objects reperesenting the days in a CalendarMonth
    Previously, all these day moment objects were being created every time the CalendarMonth was updated. Now, only those CalendarDay objects that have changed are getting updated.

--- src/components/CalendarMonth.jsx
+++ src/components/CalendarMonth.jsx
@@ -1,6 +1,7 @@
 /* eslint react/no-array-index-key: 0 */
 
 import React, { PropTypes } from 'react';
+import shallowCompare from 'react-addons-shallow-compare';
 import momentPropTypes from 'react-moment-proptypes';
 import moment from 'moment';
 import cx from 'classnames';
@@ -55,71 +56,94 @@ export function getModifiersForDay(modifiers, day) {
   return day ? Object.keys(modifiers).filter(key => modifiers[key](day)) : [];
 }
 
-export default function CalendarMonth(props) {
-  const {
-    month,
-    monthFormat,
-    orientation,
-    isVisible,
-    modifiers,
-    enableOutsideDays,
-    onDayClick,
-    onDayMouseDown,
-    onDayMouseUp,
-    onDayMouseEnter,
-    onDayMouseLeave,
-    onDayTouchStart,
-    onDayTouchEnd,
-    onDayTouchTap,
-  } = props;
-  const monthTitle = month.format(monthFormat);
+export default class CalendarMonth extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weeks: getCalendarMonthWeeks(props.month, props.enableOutsideDays),
+    };
+  }
 
-  const calendarMonthClasses = cx('CalendarMonth', {
-    'CalendarMonth--horizontal': orientation === HORIZONTAL_ORIENTATION,
-    'CalendarMonth--vertical': orientation === VERTICAL_ORIENTATION,
-  });
+  componentWillReceiveProps(nextProps) {
+    const { month, enableOutsideDays } = nextProps;
+    if (!month.isSame(this.props.month)) {
+      this.setState({
+        weeks: getCalendarMonthWeeks(month, enableOutsideDays),
+      });
+    }
+  }
 
-  return (
-    <div className={calendarMonthClasses} data-visible={isVisible}>
-      <table>
-        <caption className="CalendarMonth__caption js-CalendarMonth__caption">
-          <strong>{monthTitle}</strong>
-        </caption>
+  shouldComponentUpdate(nextProps, nextState) {
+    return shallowCompare(this, nextProps, nextState);
+  }
 
-        <tbody className="js-CalendarMonth__grid">
-          {getCalendarMonthWeeks(month, enableOutsideDays).map((week, i) => (
-            <tr key={i}>
-              {week.map((day, j) => {
-                const modifiersForDay = getModifiersForDay(modifiers, day);
-                const className = cx('CalendarMonth__day', {
-                  'CalendarMonth__day--outside': !day || day.month() !== month.month(),
-                }, modifiersForDay.map(mod => `CalendarMonth__day--${mod}`));
+  render() {
+    const {
+      month,
+      monthFormat,
+      orientation,
+      isVisible,
+      modifiers,
+      onDayClick,
+      onDayMouseDown,
+      onDayMouseUp,
+      onDayMouseEnter,
+      onDayMouseLeave,
+      onDayTouchStart,
+      onDayTouchEnd,
+      onDayTouchTap,
+    } = this.props;
 
-                return (
-                  <td className={className} key={j}>
-                    {day &&
-                      <CalendarDay
-                        day={day}
-                        modifiers={modifiersForDay}
-                        onDayMouseEnter={onDayMouseEnter}
-                        onDayMouseLeave={onDayMouseLeave}
-                        onDayMouseDown={onDayMouseDown}
-                        onDayMouseUp={onDayMouseUp}
-                        onDayClick={onDayClick}
-                        onDayTouchStart={onDayTouchStart}
-                        onDayTouchEnd={onDayTouchEnd}
-                        onDayTouchTap={onDayTouchTap}
-                      />
-                    }
-                  </td>
-                );
-              })}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    const { weeks } = this.state;
+    const monthTitle = month.format(monthFormat);
+
+    const calendarMonthClasses = cx('CalendarMonth', {
+      'CalendarMonth--horizontal': orientation === HORIZONTAL_ORIENTATION,
+      'CalendarMonth--vertical': orientation === VERTICAL_ORIENTATION,
+    });
+
+    return (
+      <div className={calendarMonthClasses} data-visible={isVisible}>
+        <table>
+          <caption className="CalendarMonth__caption js-CalendarMonth__caption">
+            <strong>{monthTitle}</strong>
+          </caption>
+
+          <tbody className="js-CalendarMonth__grid">
+            {weeks.map((week, i) => (
+              <tr key={i}>
+                {week.map((day, j) => {
+                  const modifiersForDay = getModifiersForDay(modifiers, day);
+                  const className = cx('CalendarMonth__day', {
+                    'CalendarMonth__day--outside': !day || day.month() !== month.month(),
+                  }, modifiersForDay.map(mod => `CalendarMonth__day--${mod}`));
+
+                  return (
+                    <td className={className} key={j}>
+                      {day &&
+                        <CalendarDay
+                          day={day}
+                          modifiers={modifiersForDay}
+                          onDayMouseEnter={onDayMouseEnter}
+                          onDayMouseLeave={onDayMouseLeave}
+                          onDayMouseDown={onDayMouseDown}
+                          onDayMouseUp={onDayMouseUp}
+                          onDayClick={onDayClick}
+                          onDayTouchStart={onDayTouchStart}
+                          onDayTouchEnd={onDayTouchEnd}
+                          onDayTouchTap={onDayTouchTap}
+                        />
+                      }
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 CalendarMonth.propTypes = propTypes;
