d2e2320974aa2047b800d0be1e42d1780341e644
Jack Zhang <jack.zhang>
2017-03-30 17:19:59 -0700

    Speed up isSameDay

--- src/utils/isSameDay.js
+++ src/utils/isSameDay.js
@@ -2,5 +2,8 @@ import moment from 'moment';
 
 export default function isSameDay(a, b) {
   if (!moment.isMoment(a) || !moment.isMoment(b)) return false;
-  return a.isSame(b, 'day');
+  // Speeding up by comparing most likely to differ unit first
+  return a.day() === b.day() &&
+    a.month() === b.month() &&
+    a.year() === b.year();
 }
