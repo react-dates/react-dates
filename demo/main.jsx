773dda03cfa68e69838a6e395782adcb48aba9eb
Sergey Kurdin <sergey@ski.com>
2017-04-21 10:47:29 -0400

    more demos added

--- demo/main.jsx
+++ demo/main.jsx
@@ -6,15 +6,28 @@
 // var ToggleBox = require('./examples/toggle-box');
 // var TriggerBox = require('./examples/trigger-box');
 
+import isSameDay from '../src/utils/isSameDay';
+import moment from 'moment';
+
 import { DateRangePickerWrapper } from '../stories/DateRangePicker';
 import '../css/styles.scss';
+
+const datesList = [
+  moment(),
+  moment().add(1, 'days'),
+  moment().add(3, 'days'),
+  moment().add(9, 'days'),
+  moment().add(10, 'days'),
+  moment().add(11, 'days'),
+  moment().add(12, 'days'),
+  moment().add(13, 'days'),
+];
 
 const Demo = (props) => {
   let boxStyle = {
     backgroundColor: '#efefef',
     margin: 10,
-    padding: '0 0 10px 0',
-    height: 300
+    padding: 10
   };
 
   let headingStyle = {
@@ -27,6 +40,11 @@ const Demo = (props) => {
     borderBottom: '1px solid #e5e5e5'
   };
 
+  let locale = props.locale ? props.locale : 'en';
+
+  moment.locale(locale);
+
+
   return (
     <div className="flex-box flex-row align-items-center" style={boxStyle}>
       <h3 style={headingStyle}>{props.title}</h3>
@@ -36,9 +54,60 @@ const Demo = (props) => {
 };
 
 const MainComponent = () => {
+  let mobile = window.innerWidth < 600;
   return (
     <div className="flex-box flex-wrap">
       <Demo title="DatePicker Default"><DateRangePickerWrapper /></Demo>
+      <Demo title="DatePicker with Dates">
+      <DateRangePickerWrapper
+            initialStartDate={moment().add(3, 'days')}
+            initialEndDate={moment().add(10, 'days')}
+      />
+      </Demo>
+      <Demo title="DatePicker with Custom Month">
+      <DateRangePickerWrapper
+            displayFormat="MMM D"
+            initialStartDate={moment().add(3, 'days')}
+            initialEndDate={moment().add(10, 'days')}
+      />
+      </Demo>
+      <Demo title="DatePicker with Blocked Dates">
+      <DateRangePickerWrapper
+            isDayBlocked={day1 => datesList.some(day2 => isSameDay(day1, day2))}
+      />
+      </Demo>
+      <Demo title="DatePicker with Highlighted Dates">
+      <DateRangePickerWrapper
+            isDayHighlighted={day1 => datesList.some(day2 => isSameDay(day1, day2))}
+      />
+      </Demo>
+      <Demo title="DatePicker with Minimal Nights 3">
+      <DateRangePickerWrapper
+            minimumNights={3}
+            initialStartDate={moment().add(3, 'days')}
+            initialEndDate={moment().add(10, 'days')}
+      />
+      </Demo>
+      <Demo title="DatePicker on Mobile">
+      <DateRangePickerWrapper
+            numberOfMonths={mobile ? 1: 2}
+            withPortal={mobile}
+            // withFullScreenPortal={mobile}
+            isOutsideRange={() => false}
+      />
+      </Demo>
+      <Demo title="DatePicker Localized" locale="_ru">
+      <DateRangePickerWrapper
+        showClearDates
+        startDatePlaceholderText="Туда"
+        endDatePlaceholderText="Обратно"
+        monthFormat="MMMM YYYY"
+        phrases={{
+          closeDatePicker: 'закрыть',
+          clearDates: 'отчистить',
+        }}
+      />
+      </Demo>
     </div>
   );
 };
