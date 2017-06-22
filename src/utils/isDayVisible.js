e914b004655f1063a6a9889d5cb737cb4a22c03c
geakstr <geakstr@me.com>
2017-06-23 00:15:08 +0400

    Fixed isDayVisible (fix #589)

--- src/utils/isDayVisible.js
+++ src/utils/isDayVisible.js
@@ -2,11 +2,11 @@ import isBeforeDay from './isBeforeDay';
 import isAfterDay from './isAfterDay';
 
 export default function isDayVisible(day, month, numberOfMonths, enableOutsideDays) {
-  let firstDayOfFirstMonth = month.clone().startOf('month');
+  let firstDayOfFirstMonth = month.clone().subtract(1, "month").startOf('month');
   if (enableOutsideDays) firstDayOfFirstMonth = firstDayOfFirstMonth.startOf('week');
   if (isBeforeDay(day, firstDayOfFirstMonth)) return false;
 
-  let lastDayOfLastMonth = month.clone().add(numberOfMonths - 1, 'months').endOf('month');
+  let lastDayOfLastMonth = month.clone().add(numberOfMonths, 'months').endOf('month');
   if (enableOutsideDays) lastDayOfLastMonth = lastDayOfLastMonth.endOf('week');
   return !isAfterDay(day, lastDayOfLastMonth);
 }
