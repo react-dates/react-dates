c07814a5589845c00b2ca37cae09a3f7cebf52ab
Jordan Harband <ljharb@gmail.com>
2017-07-24 01:02:38 -0700

    [Dev Deps] update `eslint`, `eslint-config-airbnb`, `eslint-plugin-import`, `eslint-plugin-jsx-a11y`, `enzyme`, `mocha`, `sinon`, `babel-preset-airbnb`, `chai`, `airbnb-js-shims`

--- src/components/SingleDatePicker.jsx
+++ src/components/SingleDatePicker.jsx
@@ -119,7 +119,7 @@ export default class SingleDatePicker extends React.Component {
       window,
       'resize',
       this.responsivizePickerPosition,
-    { passive: true },
+      { passive: true },
     );
     this.responsivizePickerPosition();
 
