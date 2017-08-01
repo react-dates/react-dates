24bca5e3ffd38ffb2dcef56cf07efc949709ecc9
gunnargylfa <gunnargylfa@gmail.com>
2017-08-01 20:30:45 +0000

    Pass onNextMonthClick and onPrevMonthClick handlers down the the SDP controller

--- src/components/SingleDatePicker.jsx
+++ src/components/SingleDatePicker.jsx
@@ -308,6 +308,8 @@ export default class SingleDatePicker extends React.Component {
       monthFormat,
       navPrev,
       navNext,
+      onPrevMonthClick,
+      onNextMonthClick,
       withPortal,
       withFullScreenPortal,
       keepOpenOnDateSelect,
@@ -352,6 +354,8 @@ export default class SingleDatePicker extends React.Component {
           initialVisibleMonth={initialVisibleMonth}
           navPrev={navPrev}
           navNext={navNext}
+          onPrevMonthClick={onPrevMonthClick}
+          onNextMonthClick={onNextMonthClick}
           renderMonth={renderMonth}
           renderDay={renderDay}
           renderCalendarInfo={renderCalendarInfo}
