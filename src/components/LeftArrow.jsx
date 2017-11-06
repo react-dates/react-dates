7eb21918799a9fbec7be615fed9d447e4c4f7c84
Jordan Harband <ljharb@gmail.com>
2017-11-05 19:28:52 -0800

    [Dev Deps] update `babel-plugin-inline-react-svg`

--- src/components/LeftArrow.jsx
+++ src/components/LeftArrow.jsx
@@ -1,4 +1,3 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
 import LeftArrow from '../svg/arrow-left.svg';
 
 export default LeftArrow;
--- src/components/RightArrow.jsx
+++ src/components/RightArrow.jsx
@@ -1,4 +1,3 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
 import RightArrow from '../svg/arrow-right.svg';
 
 export default RightArrow;
