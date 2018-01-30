1d05379c36133e0a19f03690825c9652a80ecbe3
LiranCohen <iliran11@gmail.com>
2018-01-30 17:04:03 +0200

    style fixes.

--- src/components/DayPickerSingleDateController.jsx
+++ src/components/DayPickerSingleDateController.jsx
@@ -362,10 +362,9 @@ export default class DayPickerSingleDateController extends React.Component {
         ...newVisibleDays,
         ...this.getModifiers(prevMonthVisibleDays),
       },
-    },()=>{
+    }, () => {
       onPrevMonthClick(prevMonth.clone());
     });
-
   }
 
   onNextMonthClick() {
@@ -387,7 +386,7 @@ export default class DayPickerSingleDateController extends React.Component {
         ...newVisibleDays,
         ...this.getModifiers(nextMonthVisibleDays),
       },
-    },() => {
+    }, () => {
       onNextMonthClick(newCurrentMonth.clone());
     });
   }
