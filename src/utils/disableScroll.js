9523e28dfe716ac3259198df11b55468f05f9e0c
João Vieira <joaoguerravieira@gmail.com>
2018-04-07 21:10:51 +0100

    Add disableScroll specs.
    Signed-off-by: João Vieira <joaoguerravieira@gmail.com>

--- src/utils/disableScroll.js
+++ src/utils/disableScroll.js
@@ -1,17 +1,20 @@
+const getScrollingRoot = () => document.scrollingElement || document.documentElement;
+
 /**
  * Recursively finds the scroll parent of a node. The scroll parrent of a node
  * is the closest node that is scrollable. A node is scrollable if:
  *  - it is allowed to scroll via CSS ('overflow-y' not visible or hidden);
  *  - and its children/content are "bigger" than the node's box height.
  *
+ * The root of the document always scrolls by default.
+ *
  * @param {HTMLElement} node Any DOM element.
- * @param {HTMLElement} rootNode The element to return when we reach the root.
  * @return {HTMLElement} The scroll parent element.
  */
-function getScrollParent(node, rootNode) {
+export function getScrollParent(node) {
   const parent = node.parentElement;
 
-  if (parent == null) return rootNode;
+  if (parent == null) return getScrollingRoot();
 
   const { overflowY } = window.getComputedStyle(parent);
   const canScroll = overflowY !== 'visible' && overflowY !== 'hidden';
@@ -20,7 +23,7 @@ function getScrollParent(node, rootNode) {
     return parent;
   }
 
-  return getScrollParent(parent, rootNode);
+  return getScrollParent(parent);
 }
 
 /**
@@ -32,12 +35,12 @@ function getScrollParent(node, rootNode) {
  * @param {Map<HTMLElement,string>} [acc] Accumulator map.
  * @return {Map<HTMLElement,string>} Map of ancestors with their 'overflow-y' value.
  */
-function getScrollAncestorsOverflowY(node, acc = new Map()) {
-  const rootNode = document.scrollingElement || document.documentElement;
-  const scrollParent = getScrollParent(node, rootNode);
+export function getScrollAncestorsOverflowY(node, acc = new Map()) {
+  const scrollingRoot = getScrollingRoot();
+  const scrollParent = getScrollParent(node);
   acc.set(scrollParent, scrollParent.style.overflowY);
 
-  if (scrollParent === rootNode) return acc;
+  if (scrollParent === scrollingRoot) return acc;
   return getScrollAncestorsOverflowY(scrollParent, acc);
 }
 
---
+++ test/utils/disableScroll_spec.js
@@ -0,0 +1,161 @@
+import { expect } from 'chai';
+
+import disableScroll, {
+  getScrollParent,
+  getScrollAncestorsOverflowY,
+} from '../../src/utils/disableScroll';
+
+const describeIfWindow = typeof document === 'undefined' ? describe.skip : describe;
+
+const createScrollContainer = (level = 1) => {
+  const el = document.createElement('div');
+  el.style.width = '300px';
+  el.style.height = `${level * 100}px`;
+  el.style.overflow = 'auto';
+  return el;
+};
+
+const createScrollContent = () => {
+  const el = document.createElement('div');
+  el.style.width = '100%';
+  el.style.height = '99999px';
+  return el;
+};
+
+const createTargetElement = () => {
+  const el = document.createElement('div');
+  return el;
+};
+
+describeIfWindow('#disableScroll', () => {
+  const grandParentScrollContainer = createScrollContainer(1);
+  const parentScrollContainer = createScrollContainer(2);
+  const scrollContent = createScrollContent();
+  const targetElement = createTargetElement();
+  scrollContent.appendChild(targetElement);
+
+  describe('#getScrollParent', () => {
+    describe('with no parent', () => {
+      before(() => {
+        document.body.appendChild(scrollContent);
+      });
+
+      after(() => {
+        document.body.removeChild(scrollContent);
+      });
+
+      it('returns scrolling root if no scroll parent', () => {
+        const scrollParent = getScrollParent(targetElement);
+        expect(scrollParent).to.equal(document.documentElement);
+      });
+    });
+
+    describe('with a single scroll container', () => {
+      before(() => {
+        parentScrollContainer.appendChild(scrollContent);
+        document.body.appendChild(parentScrollContainer);
+      });
+
+      after(() => {
+        parentScrollContainer.appendChild(scrollContent);
+        document.body.removeChild(parentScrollContainer);
+      });
+
+      it('returns the scroll container', () => {
+        const scrollParent = getScrollParent(targetElement);
+        expect(scrollParent).to.equal(parentScrollContainer);
+      });
+    });
+
+    describe('with multiple scroll containers', () => {
+      before(() => {
+        parentScrollContainer.appendChild(scrollContent);
+        grandParentScrollContainer.appendChild(parentScrollContainer);
+        document.body.appendChild(grandParentScrollContainer);
+      });
+
+      after(() => {
+        parentScrollContainer.removeChild(scrollContent);
+        grandParentScrollContainer.removeChild(parentScrollContainer);
+        document.body.removeChild(grandParentScrollContainer);
+      });
+
+      it('returns the closest scroll container', () => {
+        const scrollParent = getScrollParent(targetElement);
+        expect(scrollParent).to.equal(parentScrollContainer);
+      });
+    });
+  });
+
+  describe('#getScrollAncestorsOverflowY', () => {
+    before(() => {
+      parentScrollContainer.appendChild(scrollContent);
+      grandParentScrollContainer.appendChild(parentScrollContainer);
+      document.body.appendChild(grandParentScrollContainer);
+    });
+
+    after(() => {
+      parentScrollContainer.removeChild(scrollContent);
+      grandParentScrollContainer.removeChild(parentScrollContainer);
+      document.body.removeChild(grandParentScrollContainer);
+    });
+
+    it('returns a map with the overflowY of all scrollable ancestors', () => {
+      const scrollAncestorsOverflowY = getScrollAncestorsOverflowY(targetElement);
+      expect(scrollAncestorsOverflowY.size).to.equal(3); // both scroll containers + root
+
+      expect(scrollAncestorsOverflowY.has(parentScrollContainer)).to.equal(true);
+      expect(scrollAncestorsOverflowY.has(grandParentScrollContainer)).to.equal(true);
+      expect(scrollAncestorsOverflowY.has(document.documentElement)).to.equal(true);
+
+      expect(scrollAncestorsOverflowY.get(parentScrollContainer)).to.equal('auto');
+      expect(scrollAncestorsOverflowY.get(grandParentScrollContainer)).to.equal('auto');
+      // document is a special case - it doesn't have an explicit overflow property set,
+      // but scrolls by default.
+      expect(scrollAncestorsOverflowY.get(document.documentElement)).to.equal('');
+    });
+  });
+
+  describe('#disableScroll', () => {
+    before(() => {
+      parentScrollContainer.appendChild(scrollContent);
+      grandParentScrollContainer.appendChild(parentScrollContainer);
+      document.body.appendChild(grandParentScrollContainer);
+    });
+
+    after(() => {
+      parentScrollContainer.removeChild(scrollContent);
+      grandParentScrollContainer.removeChild(parentScrollContainer);
+      document.body.removeChild(grandParentScrollContainer);
+    });
+
+    describe('disable', () => {
+      it('should set all scroll ancestors overflow to hidden', () => {
+        const enableScroll = disableScroll(targetElement);
+        const scrollAncestorsOverflowY = getScrollAncestorsOverflowY(targetElement);
+
+        // eslint-disable-next-line no-restricted-syntax
+        for (const [, overflowY] of scrollAncestorsOverflowY) {
+          expect(overflowY).to.equal('hidden');
+        }
+
+        // reset to initial state
+        enableScroll();
+      });
+    });
+
+    describe('enable', () => {
+      it('should set all scroll ancestors overflow to their previous value', () => {
+        const scrollAncestorsOverflowYBefore = getScrollAncestorsOverflowY(targetElement);
+        const enableScroll = disableScroll(targetElement);
+        enableScroll();
+        const scrollAncestorsOverflowY = getScrollAncestorsOverflowY(targetElement);
+
+        // eslint-disable-next-line no-restricted-syntax
+        for (const [element, overflowY] of scrollAncestorsOverflowY) {
+          expect(scrollAncestorsOverflowYBefore.get(element)).to.equal(overflowY);
+        }
+      });
+    });
+  });
+});
