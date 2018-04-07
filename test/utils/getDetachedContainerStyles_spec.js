1c4fd96d75c02997753c7b22b5b7836392dfdbe5
João Vieira <joaoguerravieira@gmail.com>
2018-04-07 19:13:29 +0100

    Add getDetachedContainerStyles specs.

---
+++ test/utils/getDetachedContainerStyles_spec.js
@@ -0,0 +1,68 @@
+import { expect } from 'chai';
+import wrap from 'mocha-wrap';
+
+import getDetachedContainerStyles from '../../src/utils/getDetachedContainerStyles';
+
+import { OPEN_DOWN, OPEN_UP, ANCHOR_LEFT, ANCHOR_RIGHT } from '../../src/constants';
+
+const describeIfNoWindow = typeof document === 'undefined' ? describe : describe.skip;
+
+const windowWidth = 100;
+const windowHeight = 100;
+
+// Fake 30x100px element on x,y = 10,10
+const referenceElRect = {
+  top: 10,
+  bottom: 40,
+  left: 10,
+  right: 110,
+};
+const referenceEl = {
+  getBoundingClientRect() {
+    return referenceElRect;
+  },
+};
+
+describeIfNoWindow('#getDetachedContainerStyles', () => {
+  wrap()
+    .withGlobal('window', () => ({}))
+    .withOverride(() => window, 'innerWidth', () => windowWidth)
+    .withOverride(() => window, 'innerHeight', () => windowHeight)
+    .describe('with `window`', () => {
+      describe('on down-left positioning', () => {
+        it('returns translation from top-left of window to top-left of reference el', () => {
+          const styles = getDetachedContainerStyles(OPEN_DOWN, ANCHOR_LEFT, referenceEl);
+          expect(styles.transform).to.have.string(`translateX(${referenceElRect.left}px)`);
+          expect(styles.transform).to.have.string(`translateY(${referenceElRect.top}px)`);
+        });
+      });
+
+      describe('on up-left positioning', () => {
+        it('returns translation from bottom-left of window to bottom-left of reference el', () => {
+          const styles = getDetachedContainerStyles(OPEN_UP, ANCHOR_LEFT, referenceEl);
+          const offsetY = -(windowHeight - referenceElRect.bottom);
+          expect(styles.transform).to.have.string(`translateX(${referenceElRect.left}px)`);
+          expect(styles.transform).to.have.string(`translateY(${offsetY}px)`);
+        });
+      });
+
+      describe('on down-right positioning', () => {
+        it('returns translation from top-right of window to top-right of reference el', () => {
+          const styles = getDetachedContainerStyles(OPEN_DOWN, ANCHOR_RIGHT, referenceEl);
+          const offsetX = -(windowWidth - referenceElRect.right);
+          expect(styles.transform).to.have.string(`translateX(${offsetX}px)`);
+          expect(styles.transform).to.have.string(`translateY(${referenceElRect.top}px)`);
+        });
+      });
+
+      describe('on up-right positioning', () => {
+        it('returns translation from bottom-right of window to bottom-right of reference el', () => {
+          const styles = getDetachedContainerStyles(OPEN_UP, ANCHOR_RIGHT, referenceEl);
+          const offsetX = -(windowWidth - referenceElRect.right);
+          const offsetY = -(windowHeight - referenceElRect.bottom);
+          expect(styles.transform).to.have.string(`translateX(${offsetX}px)`);
+          expect(styles.transform).to.have.string(`translateY(${offsetY}px)`);
+        });
+      });
+    });
+});
