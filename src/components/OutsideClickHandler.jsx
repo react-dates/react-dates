9c5d828129f8f11af1d41345d196e0be0aec09b9
Maja Wichrowska <majapw@gmail.com>
2018-06-21 19:38:16 -0700

    Remove OutsideClickHandler export

--- src/components/OutsideClickHandler.jsx
+++
@@ -1,8 +0,0 @@
-import OutsideClickHandler from 'react-outside-click-handler';
-
-console.warn(`
-  The 'react-dates' OutsideClickHandler export is deprecated.
-  Please install and import from 'react-outside-click-handler' instead.
-`);
-
-export default OutsideClickHandler;
