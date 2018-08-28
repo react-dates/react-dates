1bec000378e029fd35adff1f9f5422e1f4e8ce5e
Damjan Gjurovski <damjan.gjurovski@gmail.com>
2018-08-28 14:25:13 +0200

    Refactor toMoment function to accept multiple input formats

--- src/utils/toMomentObject.js
+++ src/utils/toMomentObject.js
@@ -2,9 +2,9 @@ import moment from 'moment';
 
 import { DISPLAY_FORMAT, ISO_FORMAT } from '../constants';
 
-export default function toMomentObject(dateString, customFormat) {
-  const dateFormats = customFormat
-    ? [customFormat, DISPLAY_FORMAT, ISO_FORMAT]
+export default function toMomentObject(dateString, customFormats) {
+  const dateFormats = customFormats
+    ? [...customFormats, DISPLAY_FORMAT, ISO_FORMAT]
     : [DISPLAY_FORMAT, ISO_FORMAT];
 
   const date = moment(dateString, dateFormats, true);
