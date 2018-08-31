e230c612d5465e462aac04cfa189f4109ccf3a32
Joseph Cota <jcota@getaroom.com>
2018-08-31 11:16:59 -0500

    Making requested change

--- src/utils/baseClass.js
+++ src/utils/baseClass.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import shallowCompare from 'react-addons-shallow-compare';
 
-export const pureComponentAvailable = typeof React.PureComponent !== 'undefined';
-
-const baseClass = (pureComponent = pureComponentAvailable) => {
-  if (pureComponent) {
-    if (!React.PureComponent) {
-      throw new ReferenceError('withStyles() pureComponent option requires React 15.3.0 or later');
-    }
-
-    return React.PureComponent;
+class BaseClass extends (React.PureComponent || React.Component) {
+  [React.PureComponent || 'shouldComponentUpdate'](nextProps, nextState) {
+    return shallowCompare(this, nextProps, nextState);
   }
+}
 
-  return class BaseClass extends React.Component {
-    shouldComponentUpdate(nextProps, nextState) {
-      return shallowCompare(this, nextProps, nextState);
-    }
-  };
-};
-
-export default baseClass();
+export const pureComponentAvailable = typeof React.PureComponent !== 'undefined';
+export default BaseClass;
