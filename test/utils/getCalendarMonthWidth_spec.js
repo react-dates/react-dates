8493c235195921c43eee3d2abd9180f7dfd9bf47
Maja Wichrowska <majapw@gmail.com>
2018-09-12 17:40:36 -0700

    Address missing daypicker width when calendarMonthPadding theme variable is undefined

--- test/utils/getCalendarMonthWidth_spec.js
+++ test/utils/getCalendarMonthWidth_spec.js
@@ -6,4 +6,12 @@ describe('#getCalendarMonthWidth', () => {
   it('correctly calculates width for default day size of 39', () => {
     expect(getCalendarMonthWidth(39, 13)).to.equal(300);
   });
+
+  it('returns a number when padding is undefined', () => {
+    expect(Number.isNaN(getCalendarMonthWidth(39, undefined))).to.equal(false);
+  });
+
+  it('returns a number when padding is null', () => {
+    expect(Number.isNaN(getCalendarMonthWidth(39, null))).to.equal(false);
+  });
 });
