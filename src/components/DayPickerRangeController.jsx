246419c79092b38cbe15fc5fdf4599df8ba823e3
Pedro Abreu <pmig.abreu@gmail.com>
2018-09-25 15:16:04 +0100

    Init disablePrev and disableNext in component state

--- src/components/DayPickerRangeController.jsx
+++ src/components/DayPickerRangeController.jsx
@@ -221,6 +221,8 @@ export default class DayPickerRangeController extends React.PureComponent {
         chooseAvailableDate,
       },
       visibleDays,
+      disablePrev: false,
+      disableNext: false,
     };
 
     this.onDayClick = this.onDayClick.bind(this);
