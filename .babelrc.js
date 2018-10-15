86de0ff4b61c97b21fccbc041644ffc697723978
vikr01 <vik120@icloud.com>
2018-10-15 00:09:01 -0700

    switch from .babelrc to .babelrc.js

---
+++ .babelrc.js
@@ -0,0 +1,25 @@
+module.exports = (api) => {
+  api.assertVersion(7);
+
+  if (!api.env(['test', 'development', 'production', 'cjs', 'esm'])) {
+    return {};
+  }
+
+  const isTestEnv = api.env('test');
+
+  const removePropTypes = api.env(['production', 'cjs', 'esm']);
+
+  return {
+    presets: [
+      [require('babel-preset-airbnb'), { removePropTypes }],
+    ],
+    plugins: [
+      [require('babel-plugin-inline-react-svg'), { svgo: false }],
+      [
+        require('babel-plugin-transform-replace-object-assign'),
+        { moduleSpecifier: 'object.assign' },
+      ],
+      ...(isTestEnv ? [require('babel-plugin-istanbul')] : []),
+    ],
+  };
+};
