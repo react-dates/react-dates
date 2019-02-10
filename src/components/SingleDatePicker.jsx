13f5486c7cb72712e6c9527c832abc6a5cceead0
Dane David <dane@surveysparrow.com>
2019-02-11 01:52:52 +0530

    Add check for ref definition inside onFocusOut.

--- src/components/SingleDatePicker.jsx
+++ src/components/SingleDatePicker.jsx
@@ -248,7 +248,12 @@ class SingleDatePicker extends React.PureComponent {
 
   onFocusOut(e) {
     const { onFocusChange } = this.props;
-    if (this.dayPickerContainer.contains(e.relatedTarget || e.target)) return;
+    if (
+      this.dayPickerContainer
+      && this.dayPickerContainer.contains(e.relatedTarget || e.target)
+    ) {
+      return;
+    }
     onFocusChange({ focused: false });
   }
 
