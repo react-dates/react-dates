17f8d045e2a411ddb45d4fec0202d0c579ef9a4d
Ryan Lewis <ryanharrisonlewis@gmail.com>
2019-03-20 15:03:00 -0600

    adding documentation for valid IDs

--- src/utils/validIdList.js
+++ src/utils/validIdList.js
@@ -1,7 +1,7 @@
 const validIdListRegex = /(([A-Za-z]+[\w\-\:\.]*)\s*)*/;
 
 // A more complex regex is used here so that
-// all id's in an array must work. Even one incorrect
+// all IDs in an array must work. Even one incorrect
 // id in the list will cause this to return false.
 function isValidIdList(idList = '') {
   const matches = idList.match(validIdListRegex);
@@ -9,6 +9,8 @@ function isValidIdList(idList = '') {
 }
 
 // validates a list of IDs for use in aria-labelledby attributes
+// valid ID format is listed here:
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/id
 export default function (props, propName, componentName) {
   if (!isValidIdList(props[propName])) {
     return new Error(
