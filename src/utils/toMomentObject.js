c0560a5fc7ef14857507aaaddc7f9a0ff4731742
Florian Bienefelt <florian.bienefelt@gmail.com>
2019-05-15 16:24:30 +0200

    Allow more permissive validation of dates

--- src/utils/toMomentObject.js
+++ src/utils/toMomentObject.js
@@ -3,10 +3,16 @@ import moment from 'moment';
 import { DISPLAY_FORMAT, ISO_FORMAT } from '../constants';
 
 export default function toMomentObject(dateString, customFormat) {
-  const dateFormats = customFormat
-    ? [customFormat, DISPLAY_FORMAT, ISO_FORMAT]
-    : [DISPLAY_FORMAT, ISO_FORMAT];
-
+  let dateFormats = [];
+  
+  if (Array.isArray(customFormat)) {
+    dateFormats = [...customFormat, DISPLAY_FORMAT, ISO_FORMAT]
+  } else if (customFormat) {
+    dateFormats = [customFormat, DISPLAY_FORMAT, ISO_FORMAT]
+  } else {
+    dateFormats = [DISPLAY_FORMAT, ISO_FORMAT]
+  }
+  
   const date = moment(dateString, dateFormats, true);
   return date.isValid() ? date.hour(12) : null;
 }
