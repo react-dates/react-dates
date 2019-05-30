5e0c8c140e68528f84e1313d7293cca90dab44b8
Joe Lencioni <joe.lencioni@airbnb.com>
2019-05-30 09:26:09 -0700

    Optimize isDayVisible
    Cloning is a little expensive, so we want to do it as little as possible.
    Here we clone the month once and keep mutating that moment object.
    
    In my profiling this change by itself appears to reduce the amount of
    time spent in this function when updating DayPickeRangeController from
    ~16ms to ~11ms, with almost all of the time being spent in Moment clone.

--- src/utils/isDayVisible.js
+++ src/utils/isDayVisible.js
@@ -5,11 +5,23 @@ import isAfterDay from './isAfterDay';
 
 export default function isDayVisible(day, month, numberOfMonths, enableOutsideDays) {
   if (!moment.isMoment(day)) return false;
-  let firstDayOfFirstMonth = month.clone().startOf('month');
-  if (enableOutsideDays) firstDayOfFirstMonth = firstDayOfFirstMonth.startOf('week');
+
+  // Cloning is a little expensive, so we want to do it as little as possible.
+  // Here we clone the month once and keep mutating that moment object.
+  const mutableMonth = month.clone();
+
+  const firstDayOfFirstMonth = enableOutsideDays
+    ? mutableMonth.startOf('month').startOf('week')
+    : mutableMonth.startOf('month');
+
   if (isBeforeDay(day, firstDayOfFirstMonth)) return false;
 
-  let lastDayOfLastMonth = month.clone().add(numberOfMonths - 1, 'months').endOf('month');
-  if (enableOutsideDays) lastDayOfLastMonth = lastDayOfLastMonth.endOf('week');
+  const lastDayOfLastMonth = enableOutsideDays
+    // We need to call endOf('week') when enableOutsideDays is true, because we
+    // are reusing the moment object, and our earlier mutation may have moved it
+    // to a previous month. This should snap us back to a good starting place.
+    ? mutableMonth.endOf('week').add(numberOfMonths - 1, 'months').endOf('month').endOf('week')
+    : mutableMonth.add(numberOfMonths - 1, 'months').endOf('month');
+
   return !isAfterDay(day, lastDayOfLastMonth);
 }
