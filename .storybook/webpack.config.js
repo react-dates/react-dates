fcdeeb463f4ce9645cbf1e6777a14bababf06d25
Henric Trotzig <henric.trotzig@gmail.com>
2019-06-05 23:06:37 +0200

    Avoid unnecessary spread
    We can just push multiple objects into the array in one go.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,35 +2,33 @@ const path = require('path');
 
 module.exports = ({ config }) => {
   config.module.rules.push(
-    ...[
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'raw-loader', 'sass-loader'],
-        include: [path.resolve(__dirname, '../css/')],
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            query: {
-              presets: ['airbnb'],
-            },
+    {
+      test: /\.s?css$/,
+      use: ['style-loader', 'raw-loader', 'sass-loader'],
+      include: [path.resolve(__dirname, '../css/')],
+    },
+    {
+      test: /\.svg$/,
+      use: [
+        {
+          loader: 'babel-loader',
+          query: {
+            presets: ['airbnb'],
           },
-        ],
-      },
-      {
-        test: /\.jsx$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            query: {
-              presets: ['airbnb'],
-            },
+        },
+      ],
+    },
+    {
+      test: /\.jsx$/,
+      use: [
+        {
+          loader: 'babel-loader',
+          query: {
+            presets: ['airbnb'],
           },
-        ],
-      },
-    ],
+        },
+      ],
+    },
   );
   config.resolve.extensions = ['.js', '.jsx'];
   return config;
