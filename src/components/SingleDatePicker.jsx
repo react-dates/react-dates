e34fc2f7b66479cc4483239c2366957213765925
Gilbert Gilb's <gilbsgilbert@gmail.com>
2019-12-31 15:11:29 +0100

    Fix SingleDatePicker when appendToBody is enabled.
    Fixes #1550

--- src/components/SingleDatePicker.jsx
+++ src/components/SingleDatePicker.jsx
@@ -259,16 +259,7 @@ class SingleDatePicker extends React.PureComponent {
 
   onFocusOut(e) {
     const { onFocusChange } = this.props;
-    // In cases where **relatedTarget** is not null, it points to the right
-    // element here. However, in cases where it is null (such as clicking on a
-    // specific day) or it is **document.body** (IE11), the appropriate value is **event.target**.
-    //
-    // We handle both situations here by using the ` || ` operator to fallback
-    // to *event.target** when **relatedTarget** is not provided.
-    const relatedTarget = e.relatedTarget === document.body
-      ? e.target
-      : (e.relatedTarget || e.target);
-    if (this.dayPickerContainer.contains(relatedTarget)) return;
+    if (this.dayPickerContainer.contains(e.target)) return;
     onFocusChange({ focused: false });
   }
 
