caf6e545a5b16ba0242ee21bc04e786d5df02edd
Victor Lin <victorhlin@gmail.com>
2020-02-28 11:15:32 -0800

    pass prop through daypickerrangecontroller too

--- src/components/DayPickerRangeController.jsx
+++ src/components/DayPickerRangeController.jsx
@@ -75,6 +75,7 @@ const propTypes = forbidExtraProps({
   orientation: ScrollableOrientationShape,
   withPortal: PropTypes.bool,
   initialVisibleMonth: PropTypes.func,
+  initialMonthTitleHeight: PropTypes.number,
   hideKeyboardShortcutsPanel: PropTypes.bool,
   daySize: nonNegativeInteger,
   noBorder: PropTypes.bool,
@@ -151,6 +152,7 @@ const defaultProps = {
   withPortal: false,
   hideKeyboardShortcutsPanel: false,
   initialVisibleMonth: null,
+  initialMonthTitleHeight: undefined,
   daySize: DAY_SIZE,
 
   dayPickerNavigationInlineStyles: null,
@@ -1328,6 +1330,7 @@ export default class DayPickerRangeController extends React.PureComponent {
       transitionDuration,
       verticalBorderSpacing,
       horizontalMonthPadding,
+      initialMonthTitleHeight,
     } = this.props;
 
     const {
@@ -1343,6 +1346,7 @@ export default class DayPickerRangeController extends React.PureComponent {
         orientation={orientation}
         enableOutsideDays={enableOutsideDays}
         modifiers={visibleDays}
+        initialMonthTitleHeight={initialMonthTitleHeight}
         numberOfMonths={numberOfMonths}
         onDayClick={this.onDayClick}
         onDayMouseEnter={this.onDayMouseEnter}
