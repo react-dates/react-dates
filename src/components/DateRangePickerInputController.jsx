3fd714146c3007e1f75d71f2f77476951314a58c
Marc <mgm793@gmail.com>
2021-03-03 10:04:24 +0100

    Remove console log

--- src/components/DateRangePickerInputController.jsx
+++ src/components/DateRangePickerInputController.jsx
@@ -255,7 +255,6 @@ export default class DateRangePickerInputController extends React.PureComponent 
   }
 
   getDisplayFormat(date) {
-    console.log(date);
     const { displayFormat } = this.props;
     return typeof displayFormat === 'string' ? displayFormat : displayFormat(date);
   }
